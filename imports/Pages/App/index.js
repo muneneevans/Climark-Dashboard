17056f26cc7291f6d5ba00561b39e7285f49d164
muneneevans <munene.evansk@gmail.com>
2018-08-02 15:05:13 +0300

    [ADD] redux setup

--- imports/Pages/App/index.js
+++ imports/Pages/App/index.js
@@ -1,19 +1,31 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import store, { persistor } from "../../Store/configureStore";
 
 import Home from "../Home";
+
+const Routes = () => (
+  <Router>
+    <Switch>
+      <Route path="/home" component={() => <div>no mans land</div>} />
+      <Route exact path="/" component={Home} />
+    </Switch>
+  </Router>
+);
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Router>
-          <Switch>
-            <Route path="/home" component={() => <div>no mans land</div>} />
-            <Route exact path="/" component={Home} />
-          </Switch>
-        </Router>
-      </div>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <div>
+            <Routes />
+          </div>
+        </PersistGate>
+      </Provider>
     );
   }
 }
---
+++ imports/Store/Authentication/reducers.js
@@ -0,0 +1,65 @@
+import * as actionTypes from "./actionTypes";
+import * as processTypes from "../Shared/processTypes";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
+const initialState = {
+  _loginProcess: { status: processTypes.IDLE },
+  auth: {
+    isUserAuthenticated: false,
+    token: {}
+  },
+  user: {}
+};
+
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  blacklist: ["_loginProcess"]
+};
+
+export const authReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case actionTypes.LOG_IN_REQUESTED:
+      return {
+        ...state,
+        _loginProcess: { status: processTypes.PROCESSING }
+      };
+    case actionTypes.LOG_IN_FAILED:
+      return {
+        ...state,
+        _loginProcess: { status: processTypes.ERROR }
+      };
+
+    case actionTypes.LOG_IN_INVALID:
+      return {
+        ...state,
+        _loginProcess: { status: processTypes.ERROR }
+      };
+
+    case actionTypes.LOG_IN_SUCCEEDED:
+      return {
+        ...state,
+        _loginProcess: { status: processTypes.SUCCESS },
+        auth: {
+          isUserAuthenticated: true,
+          token: action.payload.token
+        },
+        user: action.payload.user
+      };
+    case actionTypes.SAVE_CREDENTIALS_REQUESTED:
+      return {
+        ...state,
+        user: action.payload
+      };
+    case actionTypes.REFRESH_AUTH_SUCCEEDED:
+      return {
+        ...state,
+        auth: { isUserAuthenticated: true, token: action.payload }
+      };
+    default:
+      return state;
+  }
+};
+
+export default persistReducer(authPersistConfig, authReducer);

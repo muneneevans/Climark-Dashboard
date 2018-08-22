a2fdd3f16130cefa1c04dc2f124eed42555c0c24
muneneevans <munene.evansk@gmail.com>
2018-08-22 17:18:17 +0300

    [UPDATE] real time foreacats

--- imports/Store/Weather/services.js
+++ imports/Store/Weather/services.js
@@ -5,7 +5,30 @@ export default class WeatherServcice {
     // const url = HOST.concat(
     //   `/v2/weather/fields/${ward.WARD_NAME}/observations`
     // );
-    const url = HOST.concat(`/v2/weather/locations/${ward.latitude},${ward.longitude}/observations`);
+    const url = HOST.concat(
+      `/v2/weather/locations/${ward.latitude},${ward.longitude}/observations`
+    );
+    const request = {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    };
+
+    return fetch(url, request)
+      .then(response => {
+        return response;
+      })
+      .catch(error => error);
+  }
+  static getForecasts(ward, accessToken) {
+    // const url = HOST.concat(
+    //   `/v2/weather/fields/${ward.WARD_NAME}/observations`
+    // );
+    const url = HOST.concat(
+      `/v2/weather/locations/${ward.latitude},${ward.longitude}/forecasts`
+    );
     const request = {
       method: "GET",
       mode: "cors",

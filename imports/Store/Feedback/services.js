9245edffb1756296e94eba9d98bee10707e320f5
Evans munene <munene.evansk@gmail.com>
2018-11-06 11:09:57 +0300

    [UPDATED] changed host

--- imports/Store/Feedback/services.js
+++ imports/Store/Feedback/services.js
@@ -2,8 +2,8 @@ import { HOST } from "../Shared/defaults";
 
 export default class WeatherServcice {
   static sendMessage(message) {
-    const url = "http://localhost:8000/api/feedback/messages/";
     // const url = "http://40.89.150.188:8000/api/feedback/messages/";
+    const url = "http://localhost:8000/api/feedback/messages/";
 
     let myHeaders = new Headers();
     myHeaders.append("content-type", "application/json");
